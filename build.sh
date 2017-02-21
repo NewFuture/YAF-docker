6ab83985c1fa89ae5de759cc82f4c4e5e391aa40
NewFuture <github@newfuture.cc>
2017-02-21 22:41:52 +0800

    modules

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 OUT_PATH="$(pwd)/docker/$1/modules"
 TAG_NAME="newfuture_$1"
 mkdir -p "$OUT_PATH"
+mkdir -p docker/php5/cli
+
 # docker run -it --rm -v"$(pwd)/$1":/run/ alpine /run/buildext.sh
 
 docker build -t $TAG_NAME "./$1/modules/"
-docker run -it --rm -v"$OUT_PATH":/modules/ $TAG_NAME cp -R "./" "/modules/"
+
+docker run -it --rm -v"$OUT_PATH":/modules/ $TAG_NAME
+
+ls -al $OUT_PATH 
+
+cp php5/Dockerfile docker/php5/cli/
