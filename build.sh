06ab67d597623eff9a9b392613d9ab595e94b73e
NewFuture <github@newfuture.cc>
2017-02-22 19:01:46 +0800

    entry

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -e
 TAG_NAME="newfuture_$1"
 CLI_PATH="$(pwd)/docker/$1/cli/"
 FPM_PATH="$(pwd)/docker/$1/fpm/"
@@ -9,19 +10,19 @@ docker build -t $TAG_NAME "./$1/modules/"
 docker run -it --rm -v"$CLI_PATH/":/newfuture/yaf/ $TAG_NAME
 
 cp script/entry.sh "$CLI_PATH"
-cp "$CLI_PATH/*" "$FPM_PATH"
+cp -R ${CLI_PATH}* "$FPM_PATH"
 
-cp script/entry.sh > "$FPM_PATH/entry.sh"
-cat script/fpm-entry.sh >> "$FPM_PATH/entry.sh"
+cp script/fpm-entry.sh "$FPM_PATH/entry.sh"
+cat script/entry.sh >> "$FPM_PATH/entry.sh"
 
 # build Dockerfile
 
 sed -e 's/${VER_NUM}/5/' \
     -e 's/${PHP_PKG}/php5-cli php5-memcache/' \
-    template/cli.Dockerfile > "$CLI_PATH/Dockerfile"
+    template/Dockerfile > "$CLI_PATH/Dockerfile"
 
-sed -e 's/${VER_NUM}/5/' \
+sed -e 's/${VER_NUM}/5/g' \
     -e 's/${PHP_PKG}/php5-fpm php5-memcache/' \
     -e 's/PORT=80/PORT=9000/' \
-    -e "s:#ENV_FOR_FPM:FPM_USER=www FPM_CONF=/etc/$1/php-fpm.conf FPM_PATH='/etc/$1/fpm-conf.d/'\:"\
+    -e "s,#ENV_FOR_FPM,ENV FPM_USER=www FPM_CONF=/etc/$1/php-fpm.conf FPM_PATH='/etc/$1/fpm-conf.d/'," \
     template/Dockerfile > "$FPM_PATH/Dockerfile"
