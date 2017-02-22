d5501bd3a11fdea042cffcbe6b2aa7bf1d4d502b
NewFuture <NewFuture@users.noreply.github.com>
2017-02-22 07:21:49 +0000

    build 2017-02-22[skip ci]

---
+++ php5/cli/entry.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CHANGE_INI(){ \
+    if [ $(cat ${PHP_INI} | grep -c "^\s*$1") -eq 0 ] ;\
+        then echo "$1=$2" >> ${PHP_INI} ;\ 
+        else sed -i "s/^\s*$1.*$/$1=$2/" ${PHP_INI}; 
+    fi; 
+} \
+&& CHANGE_INI date.timezone ${TIMEZONE} \
+&& CHANGE_INI upload_max_filesize ${MAX_UPLOAD} \
+&& CHANGE_INI cgi.fix_pathinfo 0 \
+&& CHANGE_INI display_errors 1 \
+&& CHANGE_INI display_startup_errors 1 \
+&& exec "$@"
