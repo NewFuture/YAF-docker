dad94921a2b565a44f9169ae2ad839f20755bb69
NewFuture <github@newfuture.cc>
2017-10-24 22:47:40 +0800

    update sourece

--- build.sh
+++ build.sh
@@ -6,22 +6,30 @@ TAG_NAME="newfuture_$1"
 CLI_PATH="$(pwd)/docker/$1/cli/"
 FPM_PATH="$(pwd)/docker/$1/fpm/"
 TMP_PATH="/tmp/$1/"
+APK_PKG="$1"
+PHP_EXT='yaf-3.0.5'
+ADD_EXT=''
+
+if [ "$1" = "php5" ]; then
+PHP_VER="5"
+PHP_EXT='yaf-2.3.5 && BUILD memcached-2.2.0 && BUILD redis-3.1.4 '
+ADD_EXT='&& ADD_EXT redis && ADD_EXT memcached'
+CLI_PKG="php5-cli "
+FPM_PKG="php5-fpm "
+else
+PHP_VER="7"
+APK_PKG="${APK_PKG} php7-session php7-memcached php7-redis "
+CLI_PKG="${APK_PKG}"
+FPM_PKG="${APK_PKG} php7-fpm"
+fi
+
 mkdir -p "$CLI_PATH" "$FPM_PATH" "$TMP_PATH"
 
 # build modules
-if [ "$1" = "php5" ]; then
-sed -e 's/${VER_NUM}/5/g' \
-    -e 's/${PHP_PKG}/php5/' \
-    -e 's/${YAF}/yaf-2.3.5/' \
-    -e 's/${MEMCACHED}/memcached-2.2.0/' \
+sed -e "s/\${VER_NUM}/${PHP_VER}/g" \
+    -e "s/\${PHP_PKG}/${APK_PKG}/" \
+    -e "s/\${PHP_EXT}/${PHP_EXT}/" \
     template/modules > $TMP_PATH/Dockerfile
-else
-sed -e 's/${VER_NUM}/7/g' \
-    -e 's/${PHP_PKG}/php7 php7-session/' \
-    -e 's/${YAF}/yaf-3.0.4/' \
-    -e 's/${MEMCACHED}/memcached-3.0.3/' \
-    template/modules > $TMP_PATH/Dockerfile
-fi
 
 docker build -t $TAG_NAME "$TMP_PATH"
 docker run -it --rm -v"$CLI_PATH/":/newfuture/yaf/ $TAG_NAME
@@ -33,30 +41,20 @@ cp script/fpm-entry.sh "$FPM_PATH/entry.sh"
 cat script/entry.sh >> "$FPM_PATH/entry.sh"
 
 # build Dockerfile
-
-if [ "$1" = "php5" ]; then
-sed -e 's/${VER_NUM}/5/g' \
-    -e 's/${PHP_PKG}/php5-cli/' \
+sed -e "s/\${VER_NUM}/${PHP_VER}/g" \
+    -e "s/\${PHP_PKG}/${CLI_PKG}/" \
+    -e "s,#MORE_ENV,ASSERTIONS=0," \
+    -e "s,#ADD_EXT,${ADD_EXT}," \
     template/Dockerfile > "$CLI_PATH/Dockerfile"
 
-sed -e 's/${VER_NUM}/5/g' \
-    -e 's/${PHP_PKG}/php5-fpm/' \
+sed -e "s/\${VER_NUM}/${PHP_VER}/g" \
+    -e "s/\${PHP_PKG}/${FPM_PKG}/" \
     -e 's/PORT=80/PORT=9000/' \
-    -e "s,#MORE_ENV,FPM_USER=www FPM_CONF=/etc/$1/php-fpm.conf FPM_PATH='/etc/$1/fpm.d/'," \
+    -e "s,#MORE_ENV,ASSERTIONS=0 FPM_USER=www FPM_CONF=/etc/$1/php-fpm.conf FPM_PATH='/etc/$1/fpm.d/'," \
+    -e "s/#ADD_EXT/${ADD_EXT}/" \
     template/Dockerfile > "$FPM_PATH/Dockerfile"
-else #php7
-    sed -e 's/${VER_NUM}/7/g' \
-        -e 's/${PHP_PKG}/php7 php7-session/' \
-        -e "s,#MORE_ENV,ASSERTIONS=0," \
-        template/Dockerfile > "$CLI_PATH/Dockerfile"
 
-    sed -e 's/${VER_NUM}/7/g' \
-        -e 's/${PHP_PKG}/php7-fpm php7-session/' \
-        -e 's/PORT=80/PORT=9000/' \
-        -e "s,#MORE_ENV,ASSERTIONS=0 FPM_USER=www FPM_CONF=/etc/$1/php-fpm.conf FPM_PATH='/etc/$1/fpm.d/'," \
-        -e 's,#ClEAN_TAG,\&\& ln -s /usr/sbin/php-fpm7 /usr/bin/php-fpm,' \
-        template/Dockerfile > "$FPM_PATH/Dockerfile"
-fi
+    # -e 's,#ClEAN_TAG,\&\& ln -s /usr/sbin/php-fpm7 /usr/bin/php-fpm,' \
 
 
 echo 'CMD ["/usr/bin/php-fpm","-F"]'>>"$FPM_PATH/Dockerfile"
